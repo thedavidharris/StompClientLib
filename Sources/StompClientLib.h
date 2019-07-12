f639c33a770ab9b96412289a058f2f47f61eb195
David Harris <daharr@umich.edu>
2019-07-12 16:48:07 -0400

    Proper Carthage support for dynamic libraries

---
+++ Sources/StompClientLib.h
@@ -0,0 +1,19 @@
+//
+//  StompClientLib.h
+//  StompClientLib
+//
+//  Created by Harris, David (D.A.) on 7/12/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StompClientLib.
+FOUNDATION_EXPORT double StompClientLibVersionNumber;
+
+//! Project version string for StompClientLib.
+FOUNDATION_EXPORT const unsigned char StompClientLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StompClientLib/PublicHeader.h>
+
+
